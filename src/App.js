099579f23a5003659ae7911a518936597e6b4bb1
devilmamuncn@gmail.com <mmhutsobe01@gmail.com>
2022-03-16 16:06:17 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 
+const number = 55555;
+const singer = { name: 'Dr. Mahfuz', job: 'singer' };
+const singer2 = { name: 'Eva Rahman', job: 'singer' };
+
+const singerStyle = {
+  color: 'purple',
+  backgroundColor: 'white'
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Person name="Rubel" nayika="moushumi"></Person>
+      <Person name="Bapparaj" nayika="cheka"></Person>
+      <Person name="kuber" nayika="kopila"></Person>
+      <h5>New Component. yey...</h5>
+      <p id='totocompany'>ROck mama.. React Mama...</p>
+      <Friend movie="Singham" phone="0138743"></Friend>
+      <Friend phone="0184355"></Friend>
+    </div >
   );
+}
+
+function Person(props) {
+  return (
+    <div className='person'>
+      <h1>{props.name}</h1>
+      <p>Nayika: {props.nayika}</p>
+    </div>
+  )
+}
+
+function Friend(props) {
+  console.log(props);
+  return (
+    <div className='container'>
+      <h3>Name: {props.movie}</h3>
+      <p>Phone: {props.phone}</p>
+    </div>
+  )
 }
 
 export default App;
